48ff6926542a025718364602da9f7677fe8977f4
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-06 20:16:13 +0800

    ulysse: Shim lib_fpc_tac_shared.so with libshims_binder

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/lib_fpc_tac_shared.so|vendor/lib64/lib_fpc_tac_shared.so)
+            "${PATCHELF}" --add-needed "libshims_binder.so" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
