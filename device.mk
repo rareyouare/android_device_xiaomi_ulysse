ce832a7b3260aa5480a7766b50c61b129a4bee2b
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-06 17:54:58 +0800

    ulysse: rro_overlays: Load stock power_profile

--- device.mk
+++ device.mk
@@ -8,6 +8,11 @@
 $(call inherit-product, device/xiaomi/msm8937-common/msm8937.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
+# Overlays
+PRODUCT_PACKAGES += \
+    xiaomi_ugg_overlay \
+    xiaomi_ugglite_overlay
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
